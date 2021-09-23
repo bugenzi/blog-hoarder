d19d3fff6d6c93ee5e787e404fe2e1d3d5d9dd43
bugenzi <amarbugarin@outlook.com>
2021-09-23 18:02:50 +0200

    Integrated next js, wish me luck future self

---
+++ www/client/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/* eslint-disable no-undef */
+const HomePage: React.FC = () => (
+  <div>
+    <h1>Hello, world!</h1>
+    <a href='test'>Wubalubadub dubb</a>
+  </div>
+);
+
+export default HomePage;
